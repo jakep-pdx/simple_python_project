122e1e95b768b7d72c1c2fe67d95ff500592f9cd
jakep-pdx <62121317+jakep-pdx@users.noreply.github.com>
2022-07-01 17:13:26 -0700

    test

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
+""" packaging config file """
 import os
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
+with open("requirements.txt", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
-pkg_version = "0.0.0"
+PKG_VERSION = "0.0.0"
 try:
     new_ver = os.environ["new_ver"]
     if new_ver:
-        pkg_version = new_ver
+        PKG_VERSION = new_ver
     else:
-        print("new_ver not found, using pkg_version default:", pkg_version)
+        print("new_ver not found, using PKG_VERSION default:", PKG_VERSION)
 except KeyError:
-    print("new_ver not found, using pkg_version default:", pkg_version)
+    print("new_ver not found, using PKG_VERSION default:", PKG_VERSION)
 
 setup(
     name = "simple_py_proj",
-    version = pkg_version,
+    version = PKG_VERSION,
     python_requires = ">=3.10",
-    install_requires = "", #requirements,
+    install_requires = "", #requirements.txt,
     description = "simple test project",
     pakckages = find_packages(include=["simple_py_proj", "simple_py_proj.*"]),
     entry_points = {
